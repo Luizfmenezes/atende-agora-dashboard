23f0910445d753d5e056c7d34c0b86c0af43fd0b
Luiz Felipe Fonseca Menezes <119749729+Luizfmenezes@users.noreply.github.com>
2025-05-12 23:54:31 -0300

    3090

--- src/server/index.js
+++ src/server/index.js
@@ -11,7 +11,7 @@ const atendimentosRoutes = require('./routes/atendimentos');
 
 // Initialize express app
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3090;
 
 // Middleware
 app.use(cors());
